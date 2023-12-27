5ac227502a6337dc544f37394f48ed9c5087b923
Yana-Levchyshyna <levchyshynayana@gmail.com>
2023-12-27 19:47:10 +0200

    changed directory of the Locales folder

--- src/i18n.js
+++ src/i18n.js
@@ -1,30 +1,29 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import { reactI18nextModule } from 'react-i18next';
 
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-// don't want to use this?
-// have a look at the Quick start guide
-// for passing in lng and translations on init
+import translationEN from 'locales/EN/translation.json';
+import translationUA from 'locales/UA/translation.json';
+
+// the translations
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  ua: {
+    translation: translationUA,
+  },
+};
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
-  .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
+  .use(reactI18nextModule) // passes i18n down to react-i18next
   .init({
-    fallbackLng: 'en',
-    debug: true,
+    resources,
+    lng: 'en',
+
+    keySeparator: false, // we do not use keys in form messages.welcome
 
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false, // react already safes from xss
     },
   });
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'components/App';
-import './index.css';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './components/Theme';
 import { BrowserRouter } from 'react-router-dom';
+
+import App from 'components/App';
+import './index.css';
+import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
