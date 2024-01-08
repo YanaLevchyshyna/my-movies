5c2da517e72821554ee6d5b7c72862c06bf21fd5
Yana-Levchyshyna <levchyshynayana@gmail.com>
2024-01-09 01:24:32 +0200

    added adaptive styles

--- src/components/SearchBar/SearchBar.styled.jsx
+++ src/components/SearchBar/SearchBar.styled.jsx
@@ -3,28 +3,52 @@ import { BsSearch } from 'react-icons/bs';
 
 export const FormEl = styled.form`
   position: relative;
-  width: 700px;
+  width: 333px;
   margin: 0 auto;
   top: 0;
   left: 0;
+
+  @media screen and (min-width: 768px) {
+  }
+
+  @media screen and (min-width: 1400px) {
+    width: 700px;
+  }
 `;
 
 export const InputEl = styled.input`
   width: 100%;
-  height: 42px;
+  height: 21px;
   padding: 0;
   padding-left: 10px;
   border: 2px solid #032541;
   border-radius: 30px;
-  font-size: 18px;
-  font-weight: 400;
+  font-size: ${props => props.theme.fontSizes.xs};
   outline: none;
   color: #032541;
 
   ::placeholder {
-    font-size: 18px;
+    font-size: ${props => props.theme.fontSizes.xs};
     color: ${p => p.theme.colors.placeHolder};
     opacity: 0.4;
+
+    @media screen and (min-width: 768px) {
+      font-size: ${props => props.theme.fontSizes.s};
+    }
+
+    @media screen and (min-width: 1400px) {
+      font-size: ${props => props.theme.fontSizes.m};
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    font-size: ${props => props.theme.fontSizes.s};
+    height: 36px;
+  }
+
+  @media screen and (min-width: 1400px) {
+    font-size: ${props => props.theme.fontSizes.m};
+    height: 42px;
   }
 `;
 
@@ -32,20 +56,41 @@ export const Button = styled.button`
   position: absolute;
   top: 2px;
   right: -12px;
-  width: 111px;
-  height: 42px;
+  width: 90px;
+  height: 21px;
   padding: 0;
   border: none;
   border-radius: 30px;
   background: linear-gradient(93deg, #90cea1 25%, #01b4e4 92%);
   cursor: pointer;
+
+  @media screen and (min-width: 768px) {
+    height: 36px;
+    width: 95px;
+  }
+
+  @media screen and (min-width: 1400px) {
+    height: 42px;
+    width: 111px;
+  }
 `;
+
 export const BsSearchSVG = styled(BsSearch)`
   fill: #032541;
-  width: 24px;
-  height: 24px;
+  width: 16px;
+  height: 16px;
 
   :hover {
     fill: #fff;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 18px;
+    height: 18px;
+  }
+
+  @media screen and (min-width: 1400px) {
+    width: 24px;
+    height: 24px;
   }
 `;
--- src/pages/Movies/Movies.styled.jsx
+++ src/pages/Movies/Movies.styled.jsx
@@ -4,4 +4,9 @@ export const Title = styled.h1`
   text-align: center;
   margin-bottom: 50px;
   color: #032541;
+  font-size: ${props => props.theme.fontSizes.s};
+
+  @media screen and (min-width: 768px) {
+    font-size: 32px;
+  }
 `;
