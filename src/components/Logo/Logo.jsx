05848f7fd0719db0024c1e456a401a948fa06ccf
Yana-Levchyshyna <levchyshynayana@gmail.com>
2024-01-21 17:22:46 +0200

    changed favicon and logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import { LogoEl, Box } from './Logo.styled';
 export const Logo = () => {
   return (
     <LogoEl to="/">
-      TMDB&nbsp;<Box></Box>
+      MOVIES&nbsp;<Box></Box>
     </LogoEl>
   );
 };
