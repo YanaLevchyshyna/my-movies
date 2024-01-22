169936603a313e6c693e454738a49c8dd2acfdeb
Yana-Levchyshyna <levchyshynayana@gmail.com>
2024-01-22 22:47:24 +0200

    the url-name has been changed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/my-movies">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
