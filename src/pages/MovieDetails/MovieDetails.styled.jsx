2a6cd783ec104cc5ae0180b890ea0f3ea345bd02
Yana-Levchyshyna <levchyshynayana@gmail.com>
2024-04-28 17:47:44 +0300

    the MovieDetails styles were changed

--- src/pages/MovieDetails/MovieDetails.styled.jsx
+++ src/pages/MovieDetails/MovieDetails.styled.jsx
@@ -63,11 +63,15 @@ export const MovieDetailsContainer = styled.div`
   position: relative;
   max-width: 270px;
 
-  @media screen and (min-width: 375px) {
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  /* @media screen and (min-width: 375px) {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-  }
+  } */
   @media screen and (min-width: 768px) {
     max-width: 767px;
     position: relative;
